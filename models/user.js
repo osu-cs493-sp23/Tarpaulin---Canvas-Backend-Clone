46494555cd9e416690541254853ec57cd0955056
Daksh Viradiya <85271867+DakshVR@users.noreply.github.com>
2023-06-10 22:35:46 -0700

    Updated relation in Users models

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../lib/sequelize");
+const { Submission } = require("./submission");
 const bcrpyt = require("bcryptjs");
 const User = sequelize.define("user", {
   name: { type: DataTypes.STRING, allowNull: false },
@@ -11,6 +12,13 @@ const User = sequelize.define("user", {
   role: { type: DataTypes.STRING, allowNull: false, defaultValue: false },
 });
 
+User.hasMany(Submission, { foreignKey: { allowNull: false } });
+Submission.belongsTo(User);
+
 exports.User = User;
 
+/*
+ * Export an array containing the names of fields the client is allowed to set
+ * on users.
+ */
 exports.UserClientFields = ["name", "email", "password", "role"];
