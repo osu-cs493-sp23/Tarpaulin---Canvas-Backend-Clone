507fb6327cf89a7a87b01ee1fe632f769dab5877
samaypusarla <samaypusarla1@gmail.com>
2023-06-16 02:35:06 -0700

    additions

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.use('/users', require('./users'))
 router.use("/courses", require("./courses"));
-router.use("/submissions", require("./submissions"));
+
 router.use("/assignments", require("./assignments"));
 
 module.exports = router
