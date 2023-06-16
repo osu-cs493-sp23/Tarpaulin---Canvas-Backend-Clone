23bbd17ad0b806220d3190006965486a919db40a
sdasini <sushmad1105@gmail.com>
2023-06-15 19:30:58 -0700

    Updating authorization

--- api/assignments.js
+++ api/assignments.js
@@ -2,51 +2,31 @@ const { Router } = require("express");
 const { ValidationError } = require("sequelize");
 const { Assignment, AssignmentClientFields } = require("../models/assignment");
 const { Course, CourseClientFields } = require("../models/course");
-const { instructorVerification } = require("../lib/auth");
+const { requireAuthentication } = require("../lib/auth");
 
 const router = Router();
 
-router.get("/test", (req, res, next) => {
-  res.status(200).send({ test: "test succesful , assignments" });
-});
-
 // POST Assignments
 
-router.post("/", async (req, res, next) => {
+router.post("/", requireAuthentication, async (req, res, next) => {
   try {
-    const newAssignment = await Assignment.create(req.body);
-    res.status(201).send({
-      id: newAssignment.id,
-    });
-  } catch (err) {
-    if (err instanceof ValidationError) {
-      res.status(400).send({
-        err: err.message,
+    const courseID = req.body.courseId;
+    const course = await Course.findOne({ where: { id: courseID } });
+    console.log(course);
+    if (req.user == course.instructorID || req.admin) {
+      const newAssignment = await Assignment.create(req.body);
+      res.status(201).send({
+        id: newAssignment.id,
       });
     } else {
-      next(err);
+      res.status(400).send({ error: "Unauthorized instructor." });
     }
+  } catch (err) {
+    res.status(401).send({
+      error: "Invalid Authentication Token",
+    });
   }
 });
-
-// post assignment submission with ID
-
-router.post("/:id", async (req, res, next) => {
-  try{
-    const newAssignment = await Assignment.create(req.body)
-    res.status(201).send({
-      id: newAssignment.id
-    })
-  } catch (err) {
-    if(err instanceof ValidationError) {
-      res.status(400).send({
-        err: err.message
-      })
-    } else {
-      next(err)
-    }
-  }
-})
 
 // Get assignment with id
 router.get("/:id", async (req, res, next) => {
@@ -55,51 +35,62 @@ router.get("/:id", async (req, res, next) => {
   if (assignment) {
     res.status(200).send(assignment);
   } else {
-    next();
+    res.status(404).send({
+      error: "Assignment doesn't exist",
+    });
   }
 });
 
 // modify assignment
 
-router.patch("/:id", instructorAuthorization, async (req, res, next) => {
-  const course = await Course.findByPk(req.body.courseId);
-  if (req.user == course.instructorId || req.admin) {
-    try {
+router.patch("/:id", requireAuthentication, async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const assignment = await Assignment.findByPk(id);
+    // console.log(assignment);
+    const course = await Course.findByPk(assignment.courseId);
+    // console.log(course);
+    if (req.user == course.instructorID || req.admin) {
       const result = await Assignment.update(req.body, {
         where: { id: id },
         fields: AssignmentClientFields,
       });
-      if (result[0] > 0) {
-        res.status(204).send();
+      // console.log(result);
+      if (result > 0) {
+        res.status(204).send(result);
       } else {
         next();
       }
-    } catch (e) {
-      next(e);
+    } else {
+      res.status(403).send({
+        err: "Unauthorized to access the specified resource",
+      });
     }
-  } else {
-    res.status(403).send({
-      err: "Unauthorized to access the specified resource",
-    });
+  } catch (e) {
+    next(e);
   }
 });
 
-router.delete("/:id", instructorVerification, async (req, res, next) => {
-  const course = await Course.findByPk(req.body.courseId);
-  if (req.user == course.instructorId || req.admin) {
-    try {
-      const result = await Assignment.destroy({ where: { id: id } });
+router.delete("/:id", requireAuthentication, async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const assignment = await Assignment.findByPk(id);
+    console.log(assignment);
+    const course = await Course.findByPk(assignment.courseId);
+    if (req.user == course.instructorID || req.admin) {
+      const result = await Assignment.destroy({ where: { id: req.params.id } });
+      console.log("In here");
       if (result > 0) {
         res.status(204).send();
       } else {
         next();
       }
-    } catch (e) {
-      next(e);
+    } else {
+      res.status(400).send({ error: "Unauthorized instructor." });
     }
-  } else {
-    res.status(403).send({
-      err: "Unauthorized to Delete the specified resource",
+  } catch {
+    res.status(401).send({
+      error: "Invalid Authentication Token",
     });
   }
 });
--- initDb.js
+++ initDb.js
@@ -6,11 +6,7 @@ require("dotenv").config();
 const sequelize = require("./lib/sequelize");
 const { Course } = require("./models/course");
 const courseData = require("./testdata/courses.json");
-// const { Business, BusinessClientFields } = require("./models/business");
-
-// const businessData = require("./data/businesses.json");
 
 sequelize.sync().then(async function () {
-  // await Business.bulkCreate(businessData, { fields: BusinessClientFields });
   await Course.bulkCreate(courseData);
 });
